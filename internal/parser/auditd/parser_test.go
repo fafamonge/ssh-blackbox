1f5387aa3ba2018f776ea62756746447c595431a
Rafael Monge <fafamonge@gmail.com>
2026-07-08 14:19:20 -0600

    Add auditd SSH session fixture

--- internal/parser/auditd/parser_test.go
+++ internal/parser/auditd/parser_test.go
@@ -61,3 +61,27 @@ func TestParseAuditExecveLine(t *testing.T) {
 		t.Fatalf("expected ses 16695, got %v", ev.Actor["ses"])
 	}
 }
+
+func TestParseBavariaSSHSessionFixture(t *testing.T) {
+	lines := []string{
+		`type=USER_LOGIN msg=audit(07/08/2026 18:00:28.674:3528770) : pid=2634610 uid=root auid=wagner ses=16695 msg='op=login id=wagner exe=/usr/libexec/openssh/sshd-session hostname=bavaria.hybridsync.com addr=190.5.138.94 terminal=/dev/pts/4 res=success'`,
+		`type=SYSCALL msg=audit(07/08/2026 18:00:48.134:3528891) : arch=x86_64 syscall=execve success=yes exit=0 ppid=2634891 pid=2634931 auid=wagner uid=root gid=root euid=root tty=pts5 ses=16695 comm=touch exe=/usr/bin/touch key=root_exec`,
+		`type=SYSCALL msg=audit(07/08/2026 18:00:48.136:3528892) : arch=x86_64 syscall=execve success=yes exit=0 ppid=2634891 pid=2634932 auid=wagner uid=root gid=root euid=root tty=pts5 ses=16695 comm=stat exe=/usr/bin/stat key=root_exec`,
+	}
+
+	for _, line := range lines {
+		ev, matched, err := ParseLine(line)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !matched {
+			t.Fatalf("expected line to match: %s", line)
+		}
+		if ev.Actor["ses"] != 16695 {
+			t.Fatalf("expected ses 16695, got %v", ev.Actor["ses"])
+		}
+		if ev.Actor["auid"] != "wagner" {
+			t.Fatalf("expected auid wagner, got %v", ev.Actor["auid"])
+		}
+	}
+}
